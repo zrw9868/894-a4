e5570f6b560dc002fbfe390c0ff11840a3731ddd
Ruowang Zhang <ruowangz@mit.edu>
2020-03-31 00:33:31 -0400

    Updates

---
+++ my-app/precache-manifest.c7e3446e65db6d679406992ab80668b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff21d752b21e86ee45fa1beca40e5903",
+    "url": "/894-a4/index.html"
+  },
+  {
+    "revision": "66f378c5965d53050855",
+    "url": "/894-a4/static/css/main.48ec8d9a.chunk.css"
+  },
+  {
+    "revision": "5d3f24d60c8bb243f542",
+    "url": "/894-a4/static/js/2.8d63d7f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/894-a4/static/js/2.8d63d7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f378c5965d53050855",
+    "url": "/894-a4/static/js/main.639e5d2b.chunk.js"
+  },
+  {
+    "revision": "3e117a7a351540621772",
+    "url": "/894-a4/static/js/runtime-main.92d79b11.js"
+  }
+]);
